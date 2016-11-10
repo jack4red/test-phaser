f069becf5cf94946281adca878e482122a58cf24
zhoukang <1120429511@qq.com>
2016-11-10 16:15:52 +0800

    test

--- js/gameover.js
+++ js/gameover.js
@@ -10,7 +10,7 @@ var gameover_state = {
 			scoreString = 'Your latest score: 0'
 		}
 		scoreText = game.add.text(16, 16, scoreString, { fontSize: '32px', fill: '#000' }); 
-		var text = game.add.text(400,400,"You lose!Press spacebar to start!", {font: "30px Arial",fill: "#FFFFFF"});
+		var text = game.add.text(300,400,"You lose!\rPress space to start!", {font: "30px Arial",fill: "#FFFFFF"});
 
 		var space_key = this.game.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR);
 		space_key.onDown.add(this.start,this);	
